0c688df274f9e4fb6d6f76f4d83beb62094d9cd0
Alex Calado <alex.rogerio.calado>
2019-10-22 00:47:02 -0300

    first commit

---
+++ lib/list_cell.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_fit_app/Treinos/treino.dart';
+import 'package:projeto_fit_app/Treinos/treino_detalhes.dart';
+
+class ListCell extends StatelessWidget {
+
+  final List<Treino> treinos;
+  final int index;
+
+  ListCell({this.treinos, this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ListTile(
+          title: Text(treinos[index].title),
+          onTap: () {
+            Navigator.push(context,
+                _onTap(treinos[index].title, context, treinos[index]));
+          },
+        ),
+        Divider(),
+      ],
+    );
+  }
+
+  _onTap(String title, BuildContext context, Treino treino) {
+    return Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) =>
+            TreinoDetails(
+              title: title,
+              treino: treino,
+            ))
+    );
+  }
+
+}
